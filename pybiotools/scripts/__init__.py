db07e249025aaaadc9eb2987624a1f73b525c7ca
André Santos <andremrsantos@gmail.com>
2015-01-23 14:15:18 -0300

    Initial Execution Script scope

--- pybiotools/scripts/__init__.py
+++ pybiotools/scripts/__init__.py
@@ -1,2 +1,4 @@
 from script import Script
 from file_script import FileScript
+from tcga_data_script import TCGADataScript
+from pybiotools import PyBiotools
---
+++ pybiotools/scripts/pybiotools_script.py
@@ -0,0 +1,5 @@
+from pybiotools.scripts import Script, TCGADataScript
+
+
+class PyBiotoolsScript(Script):
+    pass
